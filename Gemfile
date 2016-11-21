e1ed9ee0fa12a84b771d7d42d828188da298f002
chimame <rito.tamata@gmail.com>
2016-11-21 21:19:24 +0900

    Install rack-cors

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'devise'
 gem 'devise_token_auth'
 gem 'omniauth-twitter'
 
+gem 'rack-cors', :require => 'rack/cors'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,12 @@ module Tebukuro
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use ActionDispatch::Session::CookieStore
     config.middleware.use ActionDispatch::Flash
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
   end
 end
