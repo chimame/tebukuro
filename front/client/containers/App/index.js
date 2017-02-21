31c9b8905e151b59c0dd1076e3e0608e370a509d
chimame <rito.tamata@gmail.com>
2017-02-21 10:56:50 +0900

    Add OAuthSignInButton

--- front/client/containers/App/index.js
+++ front/client/containers/App/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import { OAuthSignInButton } from "redux-auth/default-theme"
 
 export default class App extends Component {
   render() {
     return (
       <div>
+        <OAuthSignInButton provider="github" />
         {this.props.children}
       </div>
     )
